7a7d4fd48d61e78d9ade71ac626ae48a4dda7a11
Jiří Pudil <me@jiripudil.cz>
2015-03-30 14:56:39 +0200

    RSS feedy

--- app/FrontModule/Presenters/FeedPresenter.php
+++ app/FrontModule/Presenters/FeedPresenter.php
@@ -2,13 +2,54 @@
 
 namespace Slovotepec\FrontModule\Presenters;
 
+use Kdyby\Doctrine\EntityManager;
 use Nette\Application\UI\Presenter;
 use Slovotepec\Application\UI\TBasePresenter;
+use Slovotepec\Entities\Category;
+use Slovotepec\Entities\Post;
+use Slovotepec\Entities\Queries\PostsQuery;
 
 
 class FeedPresenter extends Presenter
 {
 
 	use TBasePresenter;
+
+
+	/** @var EntityManager */
+	private $em;
+
+	/** @var Post[] */
+	private $posts;
+
+
+	public function __construct(EntityManager $em)
+	{
+		$this->em = $em;
+	}
+
+
+	public function actionDefault(Category $category = NULL)
+	{
+		$query = (new PostsQuery())
+			->onlyPublished();
+
+		if ($category !== NULL) {
+			$query->ofCategory($category);
+		}
+
+		$this->posts = $this->em->getRepository(Post::class)
+			->fetch($query)
+			->applyPaging(0, 10);
+
+		$this->getHttpResponse()->setContentType('application/xml');
+	}
+
+
+	public function renderDefault(Category $category = NULL)
+	{
+		$this->template->category = $category;
+		$this->template->posts = $this->posts;
+	}
 
 }
